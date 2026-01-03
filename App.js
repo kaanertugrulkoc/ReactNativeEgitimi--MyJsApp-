02eeba1cfda0f0f903f43497b5b74b5ec4017b6f
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-03 15:57:09 +0300

    güncelle

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
@@ -8,9 +8,10 @@ export default function App() {
       <StatusBar style="auto" />
 
       <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 5 }}>Eğitime Başladın mı ?</Text>
-      <Text style={{ color: 'red', fontWeight: 'bold' }}>Evet</Text>
+      <Text style={{ color: 'red', fontWeight: 'bold' }}>Evet !</Text>
 
 
+      <Button title="tıkla" onPress={() => alert('tıklandı')} />
 
       <View style={{
         backgroundColor: '#B8B8F5',
