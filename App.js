644f762397443c4352cf9a8caed8b7fd2d7e6657
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-04 11:41:37 +0300

    güncellendi

--- App.js
+++ App.js
@@ -20,28 +20,6 @@ export default function App() {
       <Foo />
 
 
-      <Image source={require('./assets/kule.jpg')} style={{ width: 100, height: 100 }} />
-      <Image source={{ uri: "https://avatars.githubusercontent.com/u/25131682?v=4" }} style={{ width: 100, height: 100 }} />
-
-
-      <Text style={styles.yaziTipi}>Eğitime Başladın mı ?</Text>
-      <Text style={[styles.yaziTipi, { backgroundColor: 'red', color: 'white' }]}>Evet </Text>
-
-      <Button title="Sil" onPress={sil} touchSoundDisabled={true} color="red" />
-      <Button title="tıkla" onPress={() => alert('tıklandı')} touchSoundDisabled={true} />
-
-      <View style={{
-
-      }}>
-        <Text style={[styles.yeniTip, styles.yeniTip2, { fontWeight: 'bold', color: 'blue', fontSize: 30 }]}>İletişim</Text>
-        <Text style={styles.yeniTip}>https://kaanertugrulkoc.blogspot.com/</Text>
-        <Text style={styles.yeniTip}>https://github.com/kaanertugrulkoc</Text>
-        <Text style={[styles.yeniTip, { fontWeight: 'bold', color: 'blue', fontSize: 30 }]}>+90 531 605 60 65</Text>
-      </View>
-
-
-
-
 
     </View>
 
---
+++ sayfa1.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, Image, Button } from 'react-native';
+import { styles } from './style';
+
+export const sayfa1 = () => {
+    return (
+        <View style={styles.box}>
+            <Image source={require('./assets/kule.jpg')} style={{ width: 100, height: 100 }} />
+            <Image source={{ uri: "https://avatars.githubusercontent.com/u/25131682?v=4" }} style={{ width: 100, height: 100 }} />
+
+
+            <Text style={styles.yaziTipi}>Eğitime Başladın mı ?</Text>
+            <Text style={[styles.yaziTipi, { backgroundColor: 'red', color: 'white' }]}>Evet </Text>
+
+            <Button title="Sil" onPress={sil} touchSoundDisabled={true} color="red" />
+            <Button title="tıkla" onPress={() => alert('tıklandı')} touchSoundDisabled={true} />
+
+            <View style={{
+
+            }}>
+                <Text style={[styles.yeniTip, styles.yeniTip2, { fontWeight: 'bold', color: 'blue', fontSize: 30 }]}>İletişim</Text>
+                <Text style={styles.yeniTip}>https://kaanertugrulkoc.blogspot.com/</Text>
+                <Text style={styles.yeniTip}>https://github.com/kaanertugrulkoc</Text>
+                <Text style={[styles.yeniTip, { fontWeight: 'bold', color: 'blue', fontSize: 30 }]}>+90 531 605 60 65</Text>
+            </View>
+        </View>
+    );
+};
