744608bde8ab29991c899f2124b1eac9e6c26784
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-04 10:59:06 +0300

    güncellendi

--- companents/foo/foo.js
+++ companents/foo/foo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import { styles } from './style';
 
 export const Foo = () => {
     return (
--- companents/foo/style.js
+++ companents/foo/style.js
@@ -2,6 +2,8 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
     box: {
+
+        flex: 1,
         width: 100,
         height: 100,
         backgroundColor: '#e97e7eff',
@@ -12,5 +14,54 @@ export const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
         color: 'black',
+    },
+    yaziTipi: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 5,
+        color: 'black',
+        backgroundColor: 'green',
+        width: 250,
+        height: 50,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 5,
+        padding: 5,
+        borderWidth: 1,
+        borderColor: 'black',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        flexDirection: 'row',
+
+    },
+    yeniTip: {
+        backgroundColor: '#B8B8F5',
+        width: 300,
+        height: 50,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 5,
+        padding: 5,
+        borderWidth: 1,
+        borderColor: 'black',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+
+    },
+    yeniTip2: {
+        textTransform: 'uppercase',
+        textAlign: 'center',
+
     },
 });
