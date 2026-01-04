19db71b2f6110385f44542dde7161657ac7c6bd2
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-04 16:44:53 +0300

    güncellendi

--- components/pages/sayfa2.js
+++ components/pages/sayfa2.js
@@ -8,19 +8,20 @@ export const Sayfa2 = () => {
 
 
     return (
-        <View style={styles.konteyner}>
-
-            <View style={styles.kutu}>
-
-
-
-
-            </View>
-
-
-
-
+        <View style={{ flexDirection: 'row', height: '120 ', width: '350' }}>
+            <View style={{ backgroundColor: 'red', flex: 2 }}></View>
+            <View style={{ backgroundColor: 'green', flex: 4 }}></View>
+            <View style={{ backgroundColor: 'blue', flex: 1 }}></View>
 
         </View>
+
+
+
+
+
+
+
+
+
     );
 };
