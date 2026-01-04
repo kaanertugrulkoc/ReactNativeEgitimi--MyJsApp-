281ccacecea4f6e2263b1d5ce01e9e127e6f39e8
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-04 10:50:04 +0300

    güncellendi

--- companents/foo/style.js
+++ companents/foo/style.js
@@ -2,7 +2,8 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
     box: {
-        flex: 1,
+        width: 100,
+        height: 100,
         backgroundColor: '#e97e7eff',
         alignItems: 'center',
         justifyContent: 'center',
