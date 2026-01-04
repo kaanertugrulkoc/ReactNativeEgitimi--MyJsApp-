72df15428e57ba0952421acb860c8c33b91f6d1a
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-04 10:52:32 +0300

    güncellendi

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image, Alert } from 'react-native';
 import { Foo } from './companents/foo/foo';
+import { styles } from './companents/foo/style';
 
 export default function App() {
 
