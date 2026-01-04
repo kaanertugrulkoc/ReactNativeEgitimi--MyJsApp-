abf1d7cf1b698e1da5db90cb4acfd75308fb3202
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-04 13:16:14 +0300

    güncellendi

---
+++ sayfa2.js
@@ -0,0 +1,17 @@
+import { View, Text, Image, Button, Alert } from 'react-native';
+import { styles } from './components/foo/style';
+
+export const Sayfa2 = () => {
+
+
+
+
+
+    return (
+        <View style={styles.container}>
+
+
+
+        </View>
+    );
+};
