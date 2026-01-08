57085c25b75d09e1472597f7f55fab924ab35a16
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-08 20:13:16 +0300

    güncellendi

---
+++ components/pages/sayfa3.js
@@ -0,0 +1,11 @@
+
+
+class sosyal extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text style={styles.baslik}>SOSYAL</Text>
+            </View>
+        );
+    }
+}
