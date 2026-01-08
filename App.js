b67eae068c3c502c3287bb962d9f223aab628b5b
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-08 20:39:41 +0300

    güncellendi

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { ScrollView } from 'react-native';
 import { styles } from './components/style/style';
 import { Sayfa1 } from './components/pages/sayfa1';
 import { Sayfa2 } from './components/pages/sayfa2';
-import { Sosyal } from './components/pages/sosyal';
+import Sosyal from './components/pages/sosyal';
 
 
 export default function App() {
@@ -12,10 +12,10 @@ export default function App() {
     <ScrollView contentContainerStyle={[styles.container, { flex: 1 }]}>
       <StatusBar style="auto" />
 
-
+      <Sosyal />
       <Sayfa2 />
       <Sayfa1 />
-      <Sosyal />
+
 
 
     </ScrollView>
