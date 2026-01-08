5079fc25a5234737f876ce734482144bc2f9e821
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-08 21:17:52 +0300

    güncellendi

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { styles } from './components/style/style';
 import { Sayfa1 } from './components/pages/sayfa1';
 import { Sayfa2 } from './components/pages/sayfa2';
 import Sosyal from './components/pages/sosyal';
+import Sosyal1 from './components/pages/sosyal1';
 
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
     <ScrollView contentContainerStyle={[styles.container, { flex: 1 }]}>
       <StatusBar style="auto" />
 
+      <Sosyal1 />
       <Sosyal />
       <Sayfa2 />
       <Sayfa1 />
---
+++ components/pages/sosyal1.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+
+export default function Sosyal1() {
+    return (
+        <View>
+            <Text style={stil.baslik}>Sosyalllll</Text>
+        </View>
+    );
+}
