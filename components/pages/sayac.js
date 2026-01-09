d1a86317237588bae04f335570c62d4d19d316eb
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-09 18:40:09 +0300

    güncellendi

--- components/pages/sayac.js
+++ components/pages/sayac.js
@@ -5,6 +5,7 @@ import { stil } from '../../components/style/style';
 export const Sayac = () => {
     return (
         <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+
             <Text style={stil.sayac_card}>Sayac</Text>
         </View>
     );
