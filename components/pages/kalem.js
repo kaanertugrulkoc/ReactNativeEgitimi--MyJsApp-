1f801413ae6687e18a79b034baac621b7526db68
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-10 13:37:45 +0300

    güncellendi

---
+++ components/pages/kalem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Kalem {
+    constructor() {
+        this.isim = "Kalem";
+        this.renk = "Kirmizi";
+        this.boyut = 10;
+    }
+}
