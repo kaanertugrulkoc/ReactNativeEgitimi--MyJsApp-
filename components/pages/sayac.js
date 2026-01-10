b2e8121a8a5968dd710247819e17db89c8d95360
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-10 13:23:20 +0300

    güncellendi

--- components/pages/sayac.js
+++ components/pages/sayac.js
@@ -4,6 +4,18 @@ import React, { useState } from 'react';
 
 export const Sayac = () => {
     const [sayac, setSayac] = useState(0);
+    const [yas, setYas] = useState(40);
+    const [isim, setIsim] = useState("Kaan Ertugrul");
+    const [telefonno, setTelefonno] = useState("0531 60* ** **");
+    const [data, setData] = useState(
+        {
+            id: 1,
+            isim: "Kaan Ertugrul",
+            yas: 40,
+            telefonno: "0531 60* ** **",
+            data: "2026-01-10",
+        }
+    );
 
 
     return (
