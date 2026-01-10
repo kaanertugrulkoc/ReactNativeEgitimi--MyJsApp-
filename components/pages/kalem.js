60bfe4bd163e57f5d8e8d7e0e5744d833d8fa66f
kaanertugrulkoc <poison_314@hotmail.com>
2026-01-10 13:39:10 +0300

    güncellendi

--- components/pages/kalem.js
+++ components/pages/kalem.js
@@ -6,5 +6,21 @@ class Kalem {
         this.isim = "Kalem";
         this.renk = "Kirmizi";
         this.boyut = 10;
+
+        super();
+        this.state = {
+            isim: "Kalem",
+            renk: "Kirmizi",
+            boyut: 10,
+            sayac: 0,
+        };
+    }
+
+    artir() {
+        this.setState({ sayac: this.state.sayac + 1 });
+    }
+
+    azalt() {
+        this.setState({ sayac: this.state.sayac - 1 });
     }
 }
